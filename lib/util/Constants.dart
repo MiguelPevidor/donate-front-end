4ee38ee9721dfa1f302582e7b0d8db7e57fdf101
Miguel <mpvidorcruz@gmail.com>
2025-12-01 21:18:56 -0300

    refactor: Retirada do campo de pesquisa de texto, na homepage

--- lib/util/Constants.dart
+++ lib/util/Constants.dart
@@ -1,4 +1,4 @@
 class Constants {
   /////// static const String baseUrl = "http://192.168.6.100:5020/api"; //victor casa
-  static const String baseUrl = "http://172.16.40.28:5020/api"; //Victor ifes
+  static const String baseUrl = "http://172.16.40.98:5020/api"; //Victor ifes
 }
